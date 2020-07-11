7652328c182df54e6f4129289a95fcff590cd0ab
Xavier Figueroa <xavierfigueroav@gmail.com>
2020-07-11 17:09:04 -0500

    Create script to transform embeddings result for visualization
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

---
+++ visualization/transform_for_viz.py
@@ -0,0 +1,14 @@
+import pickle
+import sys
+
+from scipy.io import savemat
+
+result = 'result/L10T50G100A1ngU_iter4.p'
+output = 'results/embs.mat'
+
+with open(result, 'rb') as pfile:
+    data = pickle.load(pfile)
+    embs = {}
+    for i, emb in enumerate(data):
+        embs['U_' + str(i)] = emb
+    savemat(output, embs, oned_as='row')
